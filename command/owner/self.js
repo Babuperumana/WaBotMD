653dd03268dc5af27fd7d9870ac4827afc4b28c3
Babuperumana <babuperumana@gmail.com>
2022-09-25 09:38:00 +0530

    Update self.js

--- command/owner/self.js
+++ command/owner/self.js
@@ -2,12 +2,12 @@ const djs = require("@discordjs/collection")
 
 module.exports = {
    name: 'self',
-   desc: 'Mengubah mode ke self (khusus owner)',
+   desc: 'Change mode to self (Owner only)',
    category: 'owner',
    async exec(msg, sock, args) {
       if (!msg.key.fromMe) return msg.reply('Khusus owner!')
       if (djs.mode === "self") return msg.reply('Already in self mode!')
       djs.mode = "self"
-      msg.replyAd('Sukses mengubah mode ke self', 'Self Mode', 'Bot sekarang hanya bisa di gunakan untuk owner')
+      msg.replyAd('Successfully change mode to self', 'Self Mode', 'The bot can now only be used for the owner')
    }
 } 
