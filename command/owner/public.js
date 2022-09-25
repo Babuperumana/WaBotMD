4262583d2c67ed43a87eeec64497b6f292d34dc5
Babuperumana <babuperumana@gmail.com>
2022-09-25 09:40:44 +0530

    Update public.js

--- command/owner/public.js
+++ command/owner/public.js
@@ -2,12 +2,12 @@ const djs = require("@discordjs/collection")
 
 module.exports = {
    name: 'public',
-   desc: 'Mengubah mode ke public (untuk semua user)',
+   desc: 'Change mode to public (for all users)',
    category: 'owner',
    async exec(msg, sock, args) {
       if (!msg.key.fromMe) return msg.reply('Khusus owner!')
       if (djs.mode === "public") return msg.reply('Already in public mode!')
       djs.mode = "public"
-      msg.replyAd('Sukses mengubah mode ke public', 'Public Mode', 'Bot sekarang bisa di gunakan untuk semua user')
+      msg.replyAd('Successfully changing the mode to public', 'Public Mode', 'Bot can now be used for all users')
    }
 } 
